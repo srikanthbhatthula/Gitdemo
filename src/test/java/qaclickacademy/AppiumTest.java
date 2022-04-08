5d67ebf80b1e1ffe91850e2359d08a5d3311a42b
srikanthbhatthula <srikanthbhatthula123@gmail.com>
2022-04-08 16:16:26 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void Nativeappandroid()
+	{
+	System.out.println("Nativeappandroid");
+	}
+
+	@Test
+	public void IosApps()
+	{
+		System.out.println("IosApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/RestAPI.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPI {
+
+	@Test
+	public void PostJira()
+	{
+      System.out.println("PostJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+	      System.out.println("deleteTwitter");
+
+	}
+}
